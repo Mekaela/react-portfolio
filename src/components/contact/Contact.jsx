f26ef1d8073adfabd1e4f2b19c022e1e24ce0cfd
Mekaela <mekaelas@hotmail.com>
2023-06-13 13:16:15 +0800

    testing git

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ import styles from './Contact.module.scss';
 const Contact = () => {
     return (
         <section className={styles.contact}>
-            <h1 className={styles.contact__title}>Doing interesting work? 
+            <h1 className={styles.contact__title}>Doing interesting work! 
             <br/>Let's <a href="mailto:mekaelas@hotmail.com?" className={styles.contact__title_chat}>chat</a></h1> 
         </section>
     )
